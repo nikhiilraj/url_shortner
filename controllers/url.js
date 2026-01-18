11bd8545487d996fa339e941ec52b9469975a859
Nikhil Raj <nikhil4rajj@gmail.com>
2026-01-18 17:40:34 +0530

    analytics added

--- controllers/url.js
+++ controllers/url.js
@@ -15,4 +15,13 @@ async function handleGenerateNewUrl(req,res) {
     return res.json({id: shortId});
 };
 
-module.exports = {handleGenerateNewUrl};
+async function handleGetAnalytics(req,res) {
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({ shortId });
+    return res.json({
+        totalClicks: result.visitHistory.length,
+        analytics:result.visitHistory,
+    });
+}
+
+module.exports = {handleGenerateNewUrl,handleGetAnalytics};
