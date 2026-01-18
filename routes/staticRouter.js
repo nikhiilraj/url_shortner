731f1ed838937967c9917ada0ebf12a1e423ddfb
Nikhil Raj <nikhil4rajj@gmail.com>
2026-01-19 01:08:07 +0530

    server side rendering completed

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const { URL } = require("../models/url");
 
 const router = express.Router();
 
-router.get("/", (req,res) => {
+router.get("/", async(req,res) => {
+    const allUrls = await URL.find({});
     return res.render("home",{
+        urls: allUrls,
         id:null,
     });
 })
