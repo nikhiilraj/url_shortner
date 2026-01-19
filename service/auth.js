8d1d446e0507386e42d26ed4c41006ea320ecff4
Nikhil Raj <nikhil4rajj@gmail.com>
2026-01-20 01:16:01 +0530

    stateless jwt auth completed

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,24 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "nikh@123"
 
-function setUser(id,user){
-    return sessionIdToUserMap.set(id,user);
+function setUser(user){
+    return jwt.sign(
+        {
+            _id: user._id,
+            email: user.email,
+        },
+        secret,
+        { expiresIn: "1h" }
+    ); 
 };
 
-function getUser(id) {
-    return sessionIdToUserMap.get(id);
+function getUser(token) {
+    if(!token) return null;
+    try {
+        return jwt.verify(token, secret);
+    } catch (err) {
+        return null;
+    }
 };
 
 module.exports={
